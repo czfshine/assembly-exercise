383b116999ae18c5dfa57b129c86a8695b01e2b0
czfshine <czfshine@outlook.com>
2018-04-03 12:33:51 +0800

    p003

---
+++ print/p003.asm
@@ -0,0 +1,52 @@
+; 8086 assembly file
+; by:czfshine
+; date: 2018/04/03 12:18:43
+
+; The Main Data segment
+DATA SEGMENT
+DATA ENDS
+
+STACK SEGMENT
+DW 256 DUP(?)
+STACK ENDS
+;entry code segment
+CODE SEGMENT
+    ASSUME CS:CODE ,DS:DATA,SS:STACK
+START:                ;entry point
+    MOV AX,DATA
+    MOV DS,AX
+    MOV AX,STACK
+    MOV SS,AX
+
+    ;00000-65525
+    MOV bx,12BCH;4796
+
+    ;use stack
+
+    ;push all
+
+    MOV SI,10 ;BASE
+
+
+    mov cx,0;
+    MOV AX,BX
+    p:
+        MOV DX,0
+        DIV SI ;dx ax / 10 = ax......dx
+        push dx
+        inc cl
+        cmp ax ,0
+    jnz p 
+
+    ;pop & print
+    mov ah,02h
+    pr:
+        pop dx
+        add dl,'0'
+        int 21H
+    loop pr
+
+    MOV AH,4CH     ;return
+    INT 21H
+CODE ENDS
+END START
