1fbc30ac541fcdd468508eadf273ff7f3ce4e526
czfshine <czfshine@outlook.com>
2018-04-06 18:26:17 +0800

    进制转换文章

---
+++ print/base_t.asm
@@ -0,0 +1,38 @@
+; 8086 assembly file
+; by:czfshine
+; date: 2018/04/06 18:16:31
+
+INCLUDE BASE.INC
+include until.inc
+; The Main Data segment
+DATA SEGMENT
+DATA ENDS
+
+STACK SEGMENT
+DW 256 DUP(?)
+STACK ENDS
+;entry code segment
+CODE SEGMENT
+    ASSUME CS:CODE ,DS:DATA,SS:STACK
+START:                ;entry point
+    MOV AX,DATA
+    MOV DS,AX
+    MOV AX,STACK
+    MOV SS,AX
+    
+    MOV BX,12345;
+
+    MOV SI,2
+
+    L:
+        CALL REBASE
+        CALL CRLF
+        INC SI
+        CMP SI,11
+    JNZ L
+
+
+    MOV AH,4CH     ;return
+    INT 21H
+CODE ENDS
+END START
